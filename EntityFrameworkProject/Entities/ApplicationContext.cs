21125a5cec8340ce40917cf26cc2506fe2178bca
Douglas Alves <douglas_alvrodrigues@outlook.com>
2019-09-26 08:11:18 -0300

    EF changes

---
+++ EntityFrameworkProject/Entities/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkProject.Entities
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
